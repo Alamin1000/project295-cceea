3ba06a97d1c9d20954f710082781248c8f622a6e
alamin <alaminhosen44000@gmail.com>
2023-02-21 03:10:35 +0600

    a

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+
+
+
+
+        $('.menu-open , .offcanvas-overlay').click(function () {
+
+            $('.offcanvas-area , .offcanvas-overlay').addClass('active');
+
+        });
+        $('.menu-close , .offcanvas-overlay').click(function () {
+
+            $('.offcanvas-area , .offcanvas-overlay').removeClass('active');
+
+        });
+
+
+
+
+
+    });
+
+     
+ 
+
+}(jQuery));
